f81dfa0be5686cfbb1abafda4af1037c40292b2f
aarush262 <aarushyadav262@gmail.com>
2025-07-17 14:17:39 +0530

    fix:remove localhost

--- server/index.js
+++ server/index.js
@@ -26,4 +26,4 @@ mongoose
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
